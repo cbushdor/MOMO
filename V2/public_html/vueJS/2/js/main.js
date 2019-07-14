8648a23e4c4696e40d857082f0c7f5f5411c4600
doreys <dorey_s@laposte.net>
2019-07-14 16:09:00 +0200

    New components added...

--- V2/public_html/vueJS/2/js/main.js
+++ V2/public_html/vueJS/2/js/main.js
@@ -1,4 +1,26 @@
-Vue.component('product',{
+Vue.component('product-details',{
+    props: {
+        details: {
+            type: Array,
+            required: true,
+        }
+    },
+    template: `
+        <ul>
+            <li v-for="detail in details">
+                {{ detail }}
+            </li>
+        </ul> 
+   `
+})
+
+Vue.component('product', {
+    props:{
+        premium: {
+            type: Boolean,
+            required: true,
+        },
+    },
     template: `
         <div class="product">
             <div class="product-image">
@@ -8,12 +30,11 @@ Vue.component('product',{
                 <h2>{{ title }}</h2>
                 <p v-if="inStock">In Stock</p>
                 <p v-else  :class="{ outOfStock: !inStock }">Out of Stock</p>
+                <p>Shipping: {{ shipping }}</p>
                 <p>{{ printBrandProduct }}</p>
-                <ul>
-                    <li v-for="detail in details">
-                         {{ detail }}
-                    </li>
-                </ul>
+                <product-details :details="details"></product-details> 
+                <product-details :details="details"></product-details> 
+
                 <div v-for="(variant,index) in variants" 
                     :key="variant.Id"
                     class="color-box"
@@ -37,7 +58,7 @@ Vue.component('product',{
             selectedVariant: 0,
             altText: "A pair of socks",
             inventory: 9,
-            details: ["80% cotton", "20% polyester", "Gender neutral"],
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
             variants: [
                 {
                     variantId: 2239,
@@ -87,9 +108,18 @@ Vue.component('product',{
         onSale() {
             return this.variants[this.selectedVariant].variantQuantity > 0
         },
+        shipping() {
+            if(this.premium){
+                return "Free"
+            }
+            return 2.99
+        },
     }
 })
 
 var app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        premium: true,
+    }
 });
